4b4166cc94e1c551c9f43cc0e6292f7cdcd8985c
Juan Miguel <jumisanbeg@gmail.com>
2016-05-23 17:43:41 +0200

    Se consigue redireccionar a las páginas con las plantillas

--- ScrumWeb/scrum/views.py
+++ ScrumWeb/scrum/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def inicio(request):
-    return render(request, 'scrum/inicio.html', {})
+    return render(request, 'inicio.html', {})
